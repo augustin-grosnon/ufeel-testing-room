cd69608e6500aa755a69e16598b9e3e70d9351b3
GUITTRE <florent.guittre@epitech.eu>
2025-09-27 14:41:47 +0900

    feat: add heartbeat singleton

---
+++ Assets/Scripts/Biometrics/HeartbeatSingleton.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[System.Serializable]
+public struct BPM
+{
+    public int bpm { get; set; }
+}
+
+public class HeartbeatSingleton
+{
+    private static HeartbeatSingleton _instance;
+    private static readonly HttpClient _httpClient = new HttpClient();
+    private const string apiEndpoint = "http://localhost:4321";
+    public int Bpm { get; private set; }
+
+    public static HeartbeatSingleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new HeartbeatSingleton();
+            }
+            
+            return _instance;
+        }
+    }
+
+    private HeartbeatSingleton()
+    {
+        Bpm = 0;
+    }
+
+    public async Task FetchBPM()
+    {
+        try
+        {
+            var response = await _httpClient.GetStringAsync(apiEndpoint);
+            BPM bpmData = JsonUtility.FromJson<BPM>(response);
+            Bpm = bpmData.bpm;
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Error fetching BPM data: " + ex.Message);
+        }
+    }
+}
