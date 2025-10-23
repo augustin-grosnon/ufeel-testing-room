33bc52e76a2831952fc6a7ff3c9bb56a20268992
GUITTRE <florent.guittre@epitech.eu>
2025-10-24 02:52:38 +0900

    feat: add text changing every second in text

--- Assets/Scripts/Biometrics/updateBPMText.cs
+++ Assets/Scripts/Biometrics/updateBPMText.cs
@@ -4,15 +4,21 @@ using TMPro;
 public class updateBPMText : MonoBehaviour
 {
     public TMP_Text _bpmText;
+    
+    private async void FetchBPMData()
+    {
+        await HeartbeatSingleton.Instance.FetchBPM();
         
+        _bpmText.text = $"BPM: {HeartbeatSingleton.Instance.Bpm.ToString()}";
+    }
+
     void Start()
     {
-        int a = 0;
+        InvokeRepeating(nameof(FetchBPMData), 0f, 1f);
     }
 
     void Update()
     {
-        await HeartbeatSingleton.Instance.FetchBPM();
-        _bpmText.text = HeartbeatSingleton.Instance.Bpm.ToString();
+        
     }
 }
